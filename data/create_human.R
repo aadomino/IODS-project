8670b49b93dc999ad4413ff15942b041e71da63d
aadomino <agata.dominowska@gmail.com>
2017-11-30 09:50:23 +0200

    update variable, add my name and date as in previous data wrangling

--- data/create_human.R
+++ data/create_human.R
@@ -1,19 +1,20 @@
-#Data wrangling
+# Data wrangling
+# R Exercise 4 (but data for the next week), 29.11.2017, Agata Dominowska
 
 library(dplyr)
 
 # "Human development" and "Gender inequality" files:
 
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
-gi <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
 
 str(hd)
 dim(hd)
 summary(hd)
 
-str(gi)
-dim(gi)
-summary(gi)
+str(gii)
+dim(gii)
+summary(gii)
 
 # There are 8 variables and 195 observations in hd, a variable describing countries ranked based on human development index.
 
@@ -25,18 +26,18 @@ colnames(hd)
 colnames(hd) <- c("hdrank", "country", "hdi", "le", "expedu", "meanedu", "gni", "gnihdi")
 colnames(hd)
 
-colnames(gi)
-colnames(gi) <- c("giirank", "country", "gii", "matmort", "adolbirthrate", "parliament", "f2edu", "m2edu", "flabour", "mlabour")
-colnames(gi)
+colnames(gii)
+colnames(gii) <- c("giirank", "country", "gii", "matmort", "adolbirthrate", "parliament", "f2edu", "m2edu", "flabour", "mlabour")
+colnames(gii)
 
 # Ratios of gender vs secondary education and labour force participation in the gi data.
 
-gi <- mutate(gi, edu2ratio = f2edu / m2edu)
-gi <- mutate(gi, labratio = flabour / mlabour)
+gii <- mutate(gii, edu2ratio = f2edu / m2edu)
+gii <- mutate(gii, labratio = flabour / mlabour)
 
 # Join the two data sets by country.
 
-human <- inner_join(hd, gi, by = "country")
+human <- inner_join(hd, gii, by = "country")
 
 # Look at the joined data.
 
